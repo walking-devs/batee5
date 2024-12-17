613aa04b19614c5499646bc6bd658b2d70b2a77a
fadysaad <49643778+fadysaad@users.noreply.github.com>
2024-12-18 00:31:41 +0200

    Update api_constants.dart to localhost:5000

--- lib/a_core/constants/api_constants.dart
+++ lib/a_core/constants/api_constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static const String baseUrl = 'http://localhost:8000';
+  static const String baseUrl = 'http://localhost:5000';
   
   // Endpoints
   static const String categories = '/categories';
